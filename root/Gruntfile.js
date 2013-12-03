e49b46652cb2d7a09726609de1b6a32282c2af56
Colin Gourlay <colin.j.gourlay@gmail.com>
2013-12-04 08:02:36 +1000

    Updated Gruntfile by specifying a single stylus compile source and adding source maps to dev JS. Updated jshintrc by removing strict warning and adding browser/node global assumptions.

--- root/Gruntfile.js
+++ root/Gruntfile.js
@@ -21,6 +21,9 @@ module.exports = function(grunt) {
 
 		browserify: {
 			dev: {
+				options: {
+					debug: true
+				},
 				files: {
 					'.dev/js/app.js': ['src/js/app.js']
 				}
@@ -35,12 +38,12 @@ module.exports = function(grunt) {
 		stylus: {
 			dev: {
 				files: {
-					'.dev/css/app.css': 'src/css/**/*.styl'
+					'.dev/css/app.css': 'src/css/app.styl'
 				}
 			},
 			dist: {
 				files: {
-					'dist/css/app.css': 'src/css/**/*.styl'
+					'dist/css/app.css': 'src/css/app.styl'
 				}
 			}
 		},
